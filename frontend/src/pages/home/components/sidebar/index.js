a66750f30f9ac449d11a34b1e408ecabcd79817b
Reginaldo <root.saitam@gmail.com>
2021-04-24 22:18:04 -0300

    ajustes

--- frontend/src/pages/home/components/sidebar/index.js
+++ frontend/src/pages/home/components/sidebar/index.js
@@ -14,17 +14,12 @@ const sidebar = () => {
                 <img src={msnLogo} alt="" />
             </AeroButton>
             <AeroButton className="button">
-                <a href="http://www.github.com/pablobion" target="_blank">
+                <a href="http://www.github.com/rmsaitam" target="_blank">
                     <AiFillGithub size="30" />
                 </a>
             </AeroButton>
             <AeroButton className="button">
-                <a href="http://www.linkedin.com/in/pablobion" target="_blank">
-                    <AiFillLinkedin size="30" color="#0e76a8" />
-                </a>
-            </AeroButton>
-            <AeroButton className="button">
-                <a href="http://www.instagram.com/pablobion" target="_blank">
+                <a href="http://www.instagram.com/reginaldomatias20" target="_blank">
                     <AiOutlineInstagram size="30" color="#E1306C" />
                 </a>
             </AeroButton>
